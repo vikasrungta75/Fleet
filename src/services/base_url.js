e2a43dd216eb9117a94029a205a0a4bcd8b9a62f
vikasrungta75 <vrungta@hotmail.com>
2026-03-10 19:09:18 +0530

    fix CORS via Vercel proxy

--- src/services/base_url.js
+++ src/services/base_url.js
@@ -1,6 +1,8 @@
+const isProduction = window.location.hostname !== 'localhost';
+
 export const API_DATABASE_URL = {
-	API_RAVITY: process.env.REACT_APP_SERVICE_URL,
-	API_REST_RAVITY: process.env.REACT_APP_REST_URL,
-	API_RAVITY_INGESTION: process.env.REACT_APP_SERVICE_URL_INGESTION,
+	API_RAVITY: isProduction ? '/cxf-proxy/' : process.env.REACT_APP_SERVICE_URL,
+	API_REST_RAVITY: isProduction ? '/rest-proxy/' : process.env.REACT_APP_REST_URL,
+	API_RAVITY_INGESTION: isProduction ? '/ingestion-proxy/' : process.env.REACT_APP_SERVICE_URL_INGESTION,
 	API_STRAPI: 'https://platform-strapi.ravity.io',
 };
